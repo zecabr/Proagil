948579d91dae19f8382aaf6d43d9d8c0ddc1acaf
Zeca <zecaoliveirabr85@gmail.com>
2020-10-01 16:13:02 -0300

    Autenticação  usuarios

--- Proagil-App/src/app/app.module.ts
+++ Proagil-App/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { TooltipModule} from 'ngx-bootstrap/Tooltip';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TooltipModule } from 'ngx-bootstrap/Tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -21,18 +21,27 @@ import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipe.pipe';
 import { EventoService } from './_services/evento.service';
 import { TituloComponent } from './_shared/titulo/titulo.component';
 
+import { LoginComponent } from './user/login/login.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { UserComponent } from './user/user.component';
+
+
+
 
 @NgModule({
   declarations: [
-      AppComponent,
-      EventosComponent,
-      NavComponent,
-      DateTimeFormatPipePipe,
-      PalestrantesComponent,
-      DashbordComponent,
-      TituloComponent,
-      ContatosComponent
-   ],
+    AppComponent,
+    EventosComponent,
+    NavComponent,
+    DateTimeFormatPipePipe,
+    PalestrantesComponent,
+    DashbordComponent,
+    TituloComponent,
+    ContatosComponent,
+    UserComponent,
+    LoginComponent,
+    RegistrationComponent
+  ],
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
